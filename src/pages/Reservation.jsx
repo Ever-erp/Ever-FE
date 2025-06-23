a653085c84bec000623b0f4d634264ddc14a001f
OhChangEun <ohchangeun@OhChangEuns-iMac.local>
2025-06-23 11:31:39 +0900

    feat: 교육장 구조도 svg파일 생성 및 hover 시 색상 변경 효과 추가 #7

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,5 +1,20 @@
+import ClassMap from "@/assets/svgs/class-map.svg?react";
+import "@/assets/styles/svg.css";
+
+const handleClick = (e) => {
+  const roomGroup = e.target.closest("g.clickable");
+  if (roomGroup) {
+    console.log("선택한 방 id:", roomGroup.id);
+  }
+};
+
 const Reservation = () => {
-  return <div>예약 페이지입니다!</div>;
+  return (
+    <div className="flex flex-row">
+      <ClassMap className="w-[800px] h-[320px]" onClick={handleClick} />
+      <div>글자</div>
+    </div>
+  );
 };
 
 export default Reservation;
