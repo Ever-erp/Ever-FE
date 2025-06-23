30991ced9983f11066332a4092bb9c4a7682d66a
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-23 09:28:19 +0900

    #1 feat : App.jsx 공지사항 페이지 관련 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Reservation from "./pages/Reservation";
 import Notice from "./pages/Notice";
 import Survey from "./pages/Survey";
 import Organization from "./pages/Organization";
-
+import SingleNotice from "./pages/SingleNotice";
+import WritePage from "./pages/WritePage";
+import OrganizationClass from "./pages/OrganizationClass";
 function App() {
   return (
     <Router>
@@ -14,8 +16,11 @@ function App() {
           <Route index element={<Calendar />} />
           <Route path="reservation" element={<Reservation />} />
           <Route path="notice" element={<Notice />} />
+          <Route path="notice/write" element={<WritePage />} />
+          <Route path="notice/:noticeId" element={<SingleNotice />} />
           <Route path="survey" element={<Survey />} />
           <Route path="organization" element={<Organization />} />
+          <Route path="organization/:classId" element={<OrganizationClass />} />
         </Route>
       </Routes>
     </Router>
