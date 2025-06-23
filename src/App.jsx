b442208cf8b6394dbd8c12dd18c1c1ff5cb35bd3
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-23 09:29:21 +0900

    #1 chore : organization 관련 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Calendar from "./pages/Calendar";
 import Reservation from "./pages/Reservation";
 import Notice from "./pages/Notice";
 import Survey from "./pages/Survey";
-import Organization from "./pages/Organization";
 import SingleNotice from "./pages/SingleNotice";
 import WritePage from "./pages/WritePage";
-import OrganizationClass from "./pages/OrganizationClass";
 function App() {
   return (
     <Router>
@@ -19,8 +17,6 @@ function App() {
           <Route path="notice/write" element={<WritePage />} />
           <Route path="notice/:noticeId" element={<SingleNotice />} />
           <Route path="survey" element={<Survey />} />
-          <Route path="organization" element={<Organization />} />
-          <Route path="organization/:classId" element={<OrganizationClass />} />
         </Route>
       </Routes>
     </Router>
