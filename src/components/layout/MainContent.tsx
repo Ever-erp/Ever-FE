25a041156fa95cf49ab5043c2e2db6a28a6acc78
OhChangEun <dwc07109@gmail.com>
2025-07-07 11:00:34 +0900

    #26 refactor : Js -> Ts 기본 레이아웃 변환 완료

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const MainContent = ({ children }) => {
+type Props = {
+  children: ReactNode;
+};
+
+const MainContent = ({ children }: Props) => {
   return (
     <main className="bg-gray-150 h-full p-3 2xl:p-5 rounded-xl">
       <div className="bg-white w-full h-full p-3 pt-0 2xl:p-5 rounded-xl overflow-y-scroll flex justify-center items-center">
